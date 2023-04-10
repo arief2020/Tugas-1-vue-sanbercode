e3163d374d84a409da2e9ddedfd57ffc5b304cb1
arief2020 <msyaifullahalarief@gmail.com>
2023-04-10 23:18:35 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h1>Hello Vue</h1>
+  <h3>Nama : Muhammad Syaifullah Al Arief</h3>
+  <h3>Kelas: Bootcamp Karyawan Batch 44</h3>
+  <Navbar 
+  :pages="pages"
+  :active-page="activePage"
+  :nav-link-click="(index) => activePage = index"
+  ></Navbar>
+  <PageViewer
+  :page="pages[activePage]"
+  ></PageViewer>
+</template>
+
+<script>
+import PageViewer from './components/PageViewer.vue';
+import Navbar from './components/Navbar.vue';
+
+export default {
+  components: {
+    PageViewer,
+    Navbar
+  },
+  data() {
+    return {
+      activePage: 0,
+      pages: [
+        {
+          link: {text: 'Home', url: 'index.html'},
+          pageTitle: 'Home Page',
+          content: 'This is the Home Content'
+        },
+        {
+          link: {text: 'About', url: 'about.html'},
+          pageTitle: 'About Page',
+          content: 'This is the About Content'
+        },
+        {
+          link: {text: 'Contact', url: 'contact.html'},
+          pageTitle: 'Contact Page',
+          content: 'This is the Contact Content'
+        },
+      ]
+    }
+  },
+  components: {
+    Navbar,
+    PageViewer
+}
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <nav>
+            <div class="container-fluid">
+                <a href="#" class="navbar-brand">My Vue</a>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li v-for="(page, index) in pages" class="nav-item" :key="index">
+                    <a 
+                    class="nav-link"
+                    :class="{active: activePage == index}"
+                    aria-current="page"
+                    :href="page.link.url"
+                    :title="`This link goes to the ${page.link.text} page`"
+                    @click="navLinkClick(index)"
+                    >{{ page.link.text }}</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <button
+                    class="btn btn-primary"
+                    @click.prevent="changeTheme()"
+                    >
+                        Toogle
+                    </button>
+                </form>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default{
+    props: ['pages', 'activePage', 'navLinkClick'],
+    data() {
+        return {
+            theme: 'light'
+        }
+    },
+    methods: {
+        changeTheme(){
+            let theme = 'light';
+            if (this.theme == 'light') {
+                theme = 'dark';
+            }
+            this.theme= theme
+        }
+    }
+}
+</script>
